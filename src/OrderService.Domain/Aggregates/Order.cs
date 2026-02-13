70a84d0523d47ad4338a2ad67bb7e471670d8c5c
coreyo-git <corey.oconnor@live.com.au>
2026-02-13 21:48:26 +1000

    feat(OrderService): Implement AddItem method in Order aggregate

--- src/OrderService.Domain/Aggregates/Order.cs
+++ src/OrderService.Domain/Aggregates/Order.cs
@@ -2,14 +2,17 @@ using OrderService.Domain.Enums;
 using OrderService.Domain.ValueObjects;
 using OrderFlow.SharedKernel.ValueObjects;
 using SharedKernel.ValueObjects;
+using System.Collections.Generic;
 
 namespace OrderService.Domain.Aggregates;
 
 public sealed class Order
 {
+	private List<OrderItem> _orderItems = new();
 	public OrderId Id { get; private set; }
-	public OrderStatus Status { get; private set; }
 	public CustomerId CustomerId { get; private set; }
+	public OrderStatus Status { get; private set; }
+	public IReadOnlyCollection<OrderItem> OrderItems => _orderItems; 
 
 	private Order(CustomerId customerId)
 	{
@@ -18,14 +21,21 @@ public sealed class Order
 		CustomerId = customerId;
 	}
 
-	public static Order Create(Guid customerId)
+	public static Order Create(CustomerId customerId)
 	{
-		return new Order(CustomerId.From(customerId));
+		return new Order(customerId);
 	}
 
 	public void UpdateOrderStatus(OrderStatus newStatus)
 	{
 		// Add validation for transitions?
 		Status = newStatus;
+	}
+
+	public void AddItem(Product product)
+	{
+		var orderItemToAdd = OrderItem.Create(Guid.NewGuid(), Id, product.Id, product.Price);
+
+		_orderItems.Add(orderItemToAdd);
 	}
 }
